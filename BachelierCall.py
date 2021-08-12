8888766d7889228e536e8678f929ca866a3fd0cc
Aion Feehan <aionfeehan@gmail.com>
2021-08-11 20:56:44 -0400

    Add GPU + test

--- BachelierCall.py
+++ BachelierCall.py
@@ -56,18 +56,44 @@ class BachelierModel:
         assert payoff_function is not None
         payoff = payoff_function(s_f, k)
         return torch.mean(payoff)
-    
 
-def test():
-    n_paths = 100000
-    s_0 = np.array([1], dtype=np.float64)
-    sigma = np.linspace(0.1, 4, 30, dtype=np.float64)
-    term_structure = np.exp(np.linspace(0, 3, 30))
-    option_type = 'call'
-    k = 1
-    t = 5
-    model = BachelierModel(s_0, sigma, term_structure, n_paths)
-    pv = model.price(k, t, option_type)
+    def get_greeks(self, k: float, t: float, option_type:str = 'call') -> dict:
+        self.s_0.grad = None
+        self.sigma.grad = None
+        pv = self.price(k, t, option_type)
+        pv.backward()
+        res_dict ={
+            'pv': pv.item(),
+            'delta': self.s_0.grad,
+            'vega':self.sigma.grad
+        }
+
+    def to_device(self, device_name: str) -> None:
+        self.s_0.to(device_name)
+        self.sigma.to(device_name)
+        self.dW.to(device_name)
+        return
+
+
+def build_test_params():
+    model_init_params = {
+        'n_paths': 100000,
+        's_0': np.array([1], dtype=np.float64),
+        'sigma_term_structure': np.linspace(0.1, 4, 30, dtype=np.float64),
+        'term_structure': np.exp(np.linspace(0, 3, 30))
+    }
+    price_params = {
+        'option_type': 'call',
+        'k': 1,
+        't': 5
+    }
+    return model_init_params, price_params
+
+
+def test_model_cpu():
+    model_init_params, price_params = build_test_params()
+    model = BachelierModel(**model_init_params)
+    pv = model.price(**price_params)
     pv.backward()
     delta = model.s_0.grad
     vega = model.sigma.grad
@@ -77,7 +103,20 @@ def test():
     return
 
 
+def test_model_gpu():
+    if torch.cuda.is_available():
+        model_init_params, price_params = build_test_params()
+        model = BachelierModel(**model_init_params)
+        model.to_device('cuda')
+        greeks = model.get_greeks(**price_params)
+        print(greeks)
+    else:
+        print('CUDA not available on this device')
+    return
+
+
 if __name__ == '__main__':
-    test()
+    test_model_cpu()
+    test_model_gpu()
 
 
